b61f31ddad46586e13657dae094c6844e0ccf5df
vijayanandkh <vijayanand_k_h@yahooo.com>
2018-02-09 17:23:12 -0500

    Initial commits
    Tweets Trending at Loc

---
+++ app/src/main/java/com/demoapps/twr/twrtrendloc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demoapps.twr.twrtrendloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
